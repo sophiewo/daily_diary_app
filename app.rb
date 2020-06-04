c72aba02a924e5f20a9d6a8d3a928eae8496d891
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-06-04 11:20:57 +0100

    app features set out

--- app.rb
+++ app.rb
@@ -1,8 +1,18 @@
 require "sinatra"
 
-class Diary < Sinatra::Base
+class DiaryApp < Sinatra::Base
+  enable :sessions
+
 get '/' do
   erb :index
+end
+
+get '/new-entry' do
+  erb :new_entry
+end
+
+post '/added' do
+  erb :added
 end
 
 run! if app_file == $0
--- spec/features/getting_started_spec.rb
+++ spec/features/getting_started_spec.rb
@@ -4,3 +4,23 @@ feature "getting to the homepage" do
     expect(page).to have_content("My Diary")
   end
 end
+
+feature "navigating to add new entry" do
+  scenario "user adds a new entry" do
+    visit ('/')
+    click_button("Create a new entry")
+    expect(page).to have_content("Title")
+    expect(page).to have_content("Fill me in")
+  end
+end
+
+feature "Adding new entry" do
+  scenario "User submits title of entry" do
+    visit ('/new-entry')
+    fill_in 'Title', with: 'My first entry'
+    click_button("Add")
+    expect(page).to have_content("You've added an entry called: My first entry")
+  end
+end
+
+  # click_button("See my entries")
